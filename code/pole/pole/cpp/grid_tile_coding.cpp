999e1ef923f7719a27545814232d310da9cb9a6e
Aaron M. de Windt <aaron.dewindt@gmail.com>
2019-02-14 01:51:01 +0100

    Last weeks progress report.

---
+++ code/pole/pole/cpp/grid_tile_coding.cpp
@@ -0,0 +1,5 @@
+//
+// Created by elvieto on 12-2-19.
+//
+
+#include "grid_tile_coding.h"
---
+++ code/pole/pole/cpp/grid_tile_coding.h
@@ -0,0 +1,14 @@
+//
+// Created by elvieto on 12-2-19.
+//
+
+#ifndef POLE_GRID_TILE_CODING_H
+#define POLE_GRID_TILE_CODING_H
+
+
+class grid_tile_coding {
+
+};
+
+
+#endif //POLE_GRID_TILE_CODING_H
