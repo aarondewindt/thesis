7988cbfe7b8612e662d9ab5b45c9dd2bc2bcd50b
adewindt <aaron.dewindt@gmail.com>
2020-02-12 01:35:25 +0100

    Added numpy example.

--- code/f16/f16/f16/__main__.py
+++ code/f16/f16/f16/__main__.py
@@ -1,5 +1,11 @@
-from f16 import sum_as_string, test
+import numpy as np
+from f16 import sum_as_string, test, axpy, mult
 
 print(sum_as_string(1, 2))
-
 test()
+
+x = np.array([1., 2., 3.])
+y = np.array([4., 5., 6.])
+print(axpy(7, x, y))
+print(mult(8, x))
+print(x)
